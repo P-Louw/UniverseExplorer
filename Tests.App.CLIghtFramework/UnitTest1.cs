ec2750bef452ea7739ec2532ae8f23d3ee6dd225
P.F.W. de Bruin <pldbrn.117@gmail.com>
2021-01-23 07:31:53 +1200

    Pre reference universeapp in app.core

---
+++ Tests.App.CLIghtFramework/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace Tests.App.CLIghtFramework
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        { }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
